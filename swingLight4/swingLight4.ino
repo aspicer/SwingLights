f6811f71f96586f1a9910800635aba8826b09b66
Sandy Spicer <aspicer@gmail.com>
2014-08-22 15:10:02 -0700

    routine class

--- swingLight4/swingLight4.ino
+++ swingLight4/swingLight4.ino
@@ -1,4 +1,5 @@
 #include <myCommon.h>
+#include <WProgram.h>
 #include <FreeSixIMU.h>   // library for IMU
 #include <FIMU_ADXL345.h>
 #include <FIMU_ITG3200.h>
@@ -8,18 +9,55 @@
 #include <stdint.h>
 #include <math.h>
 
-bool DEBUG=true;
+const int PIXELS_PER_STRIP=240;
+#define PIN 6
 
-// timing constants
-int ANG_TIME_DELAY=20; // milliseconds
-int ANG_LAST_TIME;
-int LED_TIME_DELAY=20; // milliseconds
-int LED_LAST_TIME;
+class Routine 
+{
+private:
+    static const float SWING_LENGTH = 3.3;
+    static const float PITCH_ROLL_OFFSET = 75.0;
+    float ypr[3]; // yaw pitch roll
+    void updateAngle() {
+        float angle;
+        SIXDOF->getYawPitchRoll(ypr);
+        angle = ypr[1] * sin(PITCH_ROLL_OFFSET*M_PI/180) + ypr[2] * cos(PITCH_ROLL_OFFSET*M_PI/180);
+        SWING->updatePhysics(angle);
+    };
+protected:
+    Swing *SWING;
+    FreeSixIMU *SIXDOF;
+    Adafruit_NeoPixel *STRIP;
+public:
+    Routine() {
+        SWING = new Swing(SWING_LENGTH);
+        SIXDOF = new FreeSixIMU();
+        STRIP = new Adafruit_NeoPixel(PIXELS_PER_STRIP, PIN, NEO_GRB + NEO_KHZ800);
+    };
+    void loop() {
+        for (int i = 0; i < 240; i++) {
+            STRIP->setPixelColor(i, STRIP->Color(255,0,0));
+        }
+    };
+    void setup() {
+        Serial.begin(9600);
+        Wire.begin();
+        delay(5);
+        SIXDOF->init(); //begin the IMU
+        delay(5);
+        STRIP->begin();
+        STRIP->show();
+    };
+    void start() {
+        init();
+        setup();
+        while (1) {
+            updateAngle();
+            loop();
+            STRIP->show();
+        }
 
-int counter = 0;
-void loop() {
-
-	counter = counter + 1;
+    };
 };
 
 int main() {
@@ -27,3 +65,4 @@ int main() {
 	routine->start();
 	return 0;
 }
+
